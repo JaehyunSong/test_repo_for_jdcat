c88d5b4739ecab629b487b158943b0f728fb6e24
Jaehyun Song <tintstyle@gmail.com>
2026-05-10 18:35:26 +0900

    Update setup.R

--- setup.R
+++ setup.R
@@ -2,8 +2,6 @@ options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/l
 options(pkgType = "source")
 options(install.packages.compile.from.source = "never")
 
-install.packages("rdd_0.57.tar.gz", type = "source", repos = NULL)
-
 install.packages(c("rdrobust", "gsynth", "interplot", "jpmesh"))
 
 remotes::install_github("uribo/jpndistrict", upgrade = "never")
