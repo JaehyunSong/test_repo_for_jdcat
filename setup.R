fe378a965e118cb3cd18063e5f4f19902ac95562
Jaehyun Song <tintstyle@gmail.com>
2026-05-10 19:06:21 +0900

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,8 +1,3 @@
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
 options(pkgType = "source")
 options(install.packages.compile.from.source = "never")
-
-install.packages(c("rdrobust", "gsynth", "interplot", "jpmesh"))
-
-remotes::install_github("uribo/jpndistrict", upgrade = "never")
-remotes::install_github("ropensci/rnaturalearthhires", upgrade = "never")
